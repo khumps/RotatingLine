eaf28b0f9ad1589a4d860de8417888fc1289aca5
vijayjeeva1 <vijayjeeva1@gmail.com>
2015-01-30 17:12:40 -0500

    Comiit

--- LineRotation/src/Display.java
+++ LineRotation/src/Display.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
@@ -10,24 +12,48 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
-public class Display {
+public class Display extends JPanel {
+	
+	public void drawing(){
+		repaint();
+	}
+	
+	public void paintComponent(Graphics g){
+		g.setColor(Color.BLUE);
+		g.drawLine(0,0,400,400);
+	}
+	
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Line Rotator");
 		f.setVisible(true);
 		f.setPreferredSize(new Dimension(400,400));
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+	
+		JPanel panel = new JPanel(new GridBagLayout());
 		GridBagConstraints g = new GridBagConstraints();
 		g.gridheight = 1;
 		g.gridwidth = 1;
-		g.insets = new Insets(10,10,10,10);
+		g.insets = new Insets(0,0,0,0);
 		
-		JButton start = new JButton("Start");
+		JButton rotate = new JButton("Rotate Clockwise");
 		g.gridx = 0;
 		g.gridy = 0;
-		JPanel panel = new JPanel(new GridBagLayout());
-		panel.add(start, g);
-		f.getContentPane().add(panel, BorderLayout.NORTH);	
+		panel.add(rotate, g);
+		
+		JButton rotatecc = new JButton("Rotate Clockwise");
+		g.gridx = 1;
+		g.gridy = 0;
+		panel.add(rotatecc, g);
+		
+		JButton stop = new JButton("Stop");
+		g.gridx = 3;
+		g.gridy = 0;
+		panel.add(stop, g);
+		
+		Display line = new Display();
+		f.getContentPane().add(line, BorderLayout.CENTER);
+		f.getContentPane().add(panel, BorderLayout.NORTH);
 		f.pack();
 	}
 }
+
