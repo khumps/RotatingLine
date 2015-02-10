127d97a6a665a3eb54231e0f409d1887ffc6a4c2
vijayjeeva1 <vijayjeeva1@gmail.com>
2015-02-10 16:52:45 -0500

    a

--- LineRotation/src/LineDisplay.java
+++ LineRotation/src/LineDisplay.java
@@ -22,8 +22,8 @@ public class LineDisplay extends JPanel {
 	public void paintComponent(Graphics gr){
 		super.paintComponent(gr);
 		gr.setColor(c);
-		//gr.drawLine(l.getEndPoints()[0].x,l.getEndPoints()[0].y,l.getEndPoints()[1].x,l.getEndPoints()[1].y);
-		gr.drawLine(l.getEndPoints()[0].x,l.getEndPoints()[0].y, getWidth(), getHeight());
+		gr.drawLine(l.getEndPoints()[0].x,l.getEndPoints()[0].y,l.getEndPoints()[1].x,l.getEndPoints()[1].y);
+		//gr.drawLine(l.getEndPoints()[0].x,l.getEndPoints()[0].y, getWidth(), getHeight());
 	}
 
 	public Line getL() {
